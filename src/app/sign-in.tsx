07f57af76e56683e74b509bc29cf15314fb65e14
prechy123 <bamideleprecious100@gmail.com>
2024-10-19 18:56:53 +0100

    update

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -1,10 +1,32 @@
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
+import MyTextInput from "../components/MyTextInput";
+import { useState } from "react";
+import MyButton from "../components/MyButton";
+import { Link } from "expo-router";
 
 export default function signInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Sign In</Text>
+      <Text style={styles.heading}>Login to your Board Games account</Text>
+      <View>
+        <View style={styles.inputContainer}>
+          <MyTextInput label="Email Address" text={email} setText={setEmail} />
+        </View>
+        <View style={styles.inputContainer}>
+          <MyTextInput label="Password" text={password} setText={setPassword} />
+        </View>
+        <View style={{ alignSelf: "flex-start", marginVertical: 10 }}>
+          <MyButton title="Log in" link="/" />
+        </View>
+      </View>
+      <Link href="/sign-up">
+        <Text style={{ textAlign: "left" }}>
+          Already have an account? Sign Up
+        </Text>
+      </Link>
     </View>
   );
 }
@@ -19,5 +41,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     marginBottom: 15,
+  },
+  inputContainer: {
+    width: 250,
   },
 });
--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,10 +1,32 @@
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
+import MyTextInput from "../components/MyTextInput";
+import { useState } from "react";
+import MyButton from "../components/MyButton";
+import { Link } from "expo-router";
 
-export default function signUpScreen() {
+export default function signInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Sign Up</Text>
+      <Text style={styles.heading}>Create an account with Board Games</Text>
+      <View>
+        <View style={styles.inputContainer}>
+          <MyTextInput label="Email Address" text={email} setText={setEmail} />
+        </View>
+        <View style={styles.inputContainer}>
+          <MyTextInput label="Password" text={password} setText={setPassword} />
+        </View>
+        <View style={{ alignSelf: "flex-start", marginVertical: 10 }}>
+          <MyButton title="Register" link="/" />
+        </View>
+      </View>
+      <Link href="/sign-in">
+        <Text style={{ textAlign: "left" }}>
+          Already have an account? Sign in
+        </Text>
+      </Link>
     </View>
   );
 }
@@ -19,5 +41,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     marginBottom: 15,
+  },
+  inputContainer: {
+    width: 250,
   },
 });
